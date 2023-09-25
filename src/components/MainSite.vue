0c846a9ac10d49dac6edf2aa21fd26854069200e
Priscilla <priscilla.marquez@outlook.com>
2023-09-24 19:52:55 -0500

    pagination changes

--- src/components/MainSite.vue
+++ src/components/MainSite.vue
@@ -1,14 +1,14 @@
 <script setup>
   import MainSearch from '@/components/MainSearch.vue'
   import MainCards from '@/components/MainCards.vue'
-
+  import MainPage from '@/components/MainPage.vue'
 </script>
 
 <template>
   <div class="wrapper">
-  <MainSearch />
-  <MainCards />
-
+    <MainSearch />
+    <MainCards />
+    <MainPage />
   </div>
 </template>
 
