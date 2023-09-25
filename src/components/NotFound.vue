135e50be4c87b5a4e7da00f2af84b4b3fe7d9c6a
Priscilla <priscilla.marquez@outlook.com>
2023-09-24 21:38:10 -0500

    Notfoundpage styles

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,5 +1,9 @@
 <template>
-    <h1>
-        NotFound
-    </h1>
-    </template>
+    <h2 class="not-found">404 ERROR: Page Not Found</h2>
+  </template>
+  
+  <style lang="postcss" scoped>
+    .not-found {
+      @apply pt-80 text-center font-mono text-7xl font-bold text-red-600;
+    }
+  </style>
